db0e064f0fe0b2660a2f2dccd55f5f9699bad008
MatDev <cod4882@gmail.com>
2024-03-03 12:17:49 +0100

    q

--- Lez6/arraycasualeMultipli10.cpp
+++ Lez6/arraycasualeMultipli10.cpp
@@ -1,8 +1,3 @@
-/*genera array 100 elementi di numeri casuali [-100.100]
-
-visualizzare posizione e valore dei soli multipli di 10
-*/
-
 #include <iostream>
 #include <ctime>
 #include <cstdlib>
@@ -14,56 +9,42 @@ int casuale(int inf, int sup) {
     return rand() % (sup - inf + 1) + inf;
 }
 
-//definizione
 void stampaArray(int v[], int dimensione){
-	for (int i = 0; i < dimensione; i++) {
-        cout <<i<<": "<< v[i] << " "<<endl;
+    for (int i = 0; i < dimensione; i++) {
+        cout << i << ": " << v[i] << " " << endl;
     }
 }
 
 int ricercaArray(int v[], int dimensione, int elemento){
-	
-	for (int i = 0; i < dimensione; i++) 
-        if (elemento == v[i]) 
-			return i;
-	return -1;
-    
+    for (int i = 0; i < dimensione; i++) {
+        if (elemento == v[i]) {
+            return i;
+        }
+    }
+    return -1;
 }
-
-
 
 int main() {
     srand(time(NULL));
 
-    int n = 100, a = - 100, b = 100, cerca;
-    int pos = -1;
+    int n = 100, a = -100, b = 100, cerca = 0;
+    int pos;
 
     int numeri[n];
 
-    for (int i = 0; i < n; i++) {s
+    for (int i = 0; i < n; i++) {
         numeri[i] = casuale(a, b);
     }
 
-    for (int i = 0; i < n; i++) {
-        cout <<i<<": "<< numeri[i] << " "<<endl;
-    }
+    stampaArray(numeri, n);
+
     cout << endl;
 
-	 for (int i = 0; i < n; i++) {
-        if (cerca == numeri[i] && numeri[i]  % 10 == 0) { 
-            cout<<"elemento "<<cerca<<"posizione "<<i<<endl;
-		break;
+    for (int i = 0; i < n; i++) {
+        if (numeri[i] % 10 == 0) {
+            cout << "Elemento " << numeri[i] << " posizione " << i << endl;
         }
     }
-
-
-    if (pos % 10 != 0 ) {
-        cout << "Elemento non trovato" << endl;
-    } else {
-    	cout << "Posizione del numero: " << pos << endl;
-	} 
-	
-	
 
 
 }
