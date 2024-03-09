6a6359ad1a2b5465b3e96c92026c80edfe6b000c
MatDev <cod4882@gmail.com>
2024-03-09 19:37:22 +0100

    a

---
+++ EsCasa/arrayBidimensionali/diagonale.cpp
@@ -0,0 +1,37 @@
+/******************************************************************************
+
+
+
+*******************************************************************************/
+
+#include <iostream>
+#include <ctime>
+#include <cstdlib>
+#include <iomanip>
+
+using namespace std;
+
+int main() {
+
+    int m [5][5];
+
+    for(int i = 0; i < 5; i++){
+        for(int j = 0; j < 5; j++){
+            if (i == j){
+                m[i][j] = 1;
+            } else {
+                m[i][j] = 0;
+            }
+        }
+    }
+
+    for (int i = 0; i < 5;i++){
+        for (int j = 0; j < 5; j++){
+            cout<<m[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+
+
+
+}
