716dd50086033b193b25e8682d3419058600918f
MatDevYou <cod4882@gmail.com>
2024-03-27 10:25:52 +0100

    a

--- Lez11/Persona.cpp
+++ Lez11/Persona.cpp
@@ -4,53 +4,61 @@
 using namespace std;
 
 struct data {
-    int giorno;
-    int mese;
-    int anno;
+	int giorno;
+	int mese;
+	int anno;
 };
 
 struct persona {
-    string nome;
-    string cognome;
-    struct data datanascita; // Aggiunto campo per la data di nascita
-    string luogo;
-    char sesso;
+	string nome;
+	string cognome;
+	struct data datanascita; // Aggiunto campo per la data di nascita
+	string luogo;
+	char sesso;
 };
 
-void stampaPersona(struct persona x, string separatore) {
-    cout << "\nNome: " << x.nome << separatore;
-    cout << "Cognome: " << x.cognome << separatore;
-    cout << "Data di Nascita: " << x.datanascita.giorno << "/" << x.datanascita.mese << "/" << x.datanascita.anno << separatore;
-    cout << "Luogo: " << x.luogo << separatore;
-    cout << "Sesso: " << x.sesso << separatore;
+bool maggiorenne(struct persona x) {
+	return (2024 - x.datanascita.anno) >= 18;
 }
 
-bool maggiorenne(struct persona x) {
-    return (2024 - x.datanascita.anno) >= 18;
+void stampaEta(struct persona x) {
+	int eta = 2024 - x.datanascita.anno;
+	cout << "questa e' l'eta: " << eta << endl;
 }
+
+void stampaPersona(struct persona x, string separatore) {
+	cout << "\nNome: " << x.nome << separatore;
+	cout << "Cognome: " << x.cognome << separatore;
+	cout << "Data di Nascita: " << x.datanascita.giorno << "/" << x.datanascita.mese << "/" << x.datanascita.anno << separatore;
+	cout << "Luogo: " << x.luogo << separatore;
+	cout << "Sesso: " << x.sesso << separatore;
+	stampaEta(x);
+	if (maggiorenne(x))
+		cout << x.nome << " " << x.cognome << " e'  maggiorenne" << endl;
+	else
+		cout << x.nome << " " << x.cognome << " e' minorenne" << endl;
+
+}
+
 
 void personaBlocco(struct persona x) {
-    stampaPersona(x, "\n");
+	stampaPersona(x, "\n");
 }
 
+
+
 int main() {
-    struct data data_nascita = {15, 10, 1990}; // Definizione della data di nascita
-    persona p1 = {"Luca", "Rossi", data_nascita, "Torino", 'M'};
-    
-    struct data data_nascita1 = {15, 10, 2010}; // Definizione della data di nascita
-    persona p2 = {"Maria", "Angelina", data_nascita1, "Padova", 'F'};
-    
-    personaBlocco(p1);
-    if (maggiorenne(p1))
-        cout << "Luca Rossi e maggiorenne" << endl;
-    else
-        cout << "Luca Rossi e minorenne" << endl;
+	
+	
+	struct data data_nascita = {15, 10, 1990}; // Definizione della data di nascita
+	persona p1 = {"Luca", "Rossi", data_nascita, "Torino", 'M'};
 
-    personaBlocco(p2);
-    if (maggiorenne(p2))
-        cout << "Maria Angelina e maggiorenne" << endl;
-    else
-        cout << "Maria Angelina e minorenne" << endl;
+	struct data data_nascita1 = {15, 10, 2010}; // Definizione della data di nascita
+	persona p2 = {"Maria", "Angelina", data_nascita1, "Padova", 'F'};
 
-    return 0;
+	personaBlocco(p1);
+	
+	personaBlocco(p2);
+
+
 }
