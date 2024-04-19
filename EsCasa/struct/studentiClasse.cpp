d622051bdfce2a3cc7c7a188177047eedb34e3fe
MatDev <cod4882@gmail.com>
2024-04-19 10:25:54 +0200

    a

---
+++ EsCasa/struct/studentiClasse.cpp
@@ -0,0 +1,43 @@
+/******************************************************************************
+
+Definire una struct livelloLinguistico che memorizzi 
+il livello del certificato linguistico di uno studente; 
+il livello C( definito da una lettera e da un numero 
+(ad esempio: A1, B2, C2).
+Creare una struct classe definita dall'anno e dalla sezione 
+(5F, 3C, ...).
+Creare una struct studente con codice numerico studente, 
+classe e livello linguistico.
+Memorizzare 5 studenti 
+(leggendo i dati da tastiera o da un file input.txt) 
+e verificare se c'C( qualche studente di una classe 
+inferiore che ha un livello linguistico piC9 alto di uno 
+studente di una classe superiore e nel caso stamparne i dati. 
+
+*******************************************************************************/
+
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+struct certificato {
+    char lettera;
+    int numero;
+};
+
+struct classe {
+    int anno;
+    char sezione;
+};
+
+struct classe {
+    int anno;
+    char sezione;
+};
+
+
+int main(){
+
+
+}
